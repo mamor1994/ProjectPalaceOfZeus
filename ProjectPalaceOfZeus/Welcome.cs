525d1faae9a9990fde53b5812122a07c74ddf817
mppl2205 <mppl2205@unipi.gr>
2023-10-09 04:28:18 +0300

    add page close #39

--- ProjectPalaceOfZeus/Welcome.cs
+++ ProjectPalaceOfZeus/Welcome.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
+
 
 namespace ProjectPalaceOfZeus
 {
     public partial class Welcome : Form
     {
+
+        private SpeechSynthesizer synthesizer;
+        private bool needToExitApp = true;
+
         public Welcome()
         {
             InitializeComponent();
+            timer1.Interval = 4000;
+            timer1.Start();
+            synthesizer = new SpeechSynthesizer();
+            string textToSpeak = "Hello";
+            synthesizer.SpeakAsync(textToSpeak);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            synthesizer.Dispose();
+            this.Hide();
+            MainHall form = new MainHall();
+            form.Show();
+        }
+
+        private void Welcome_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (needToExitApp)
+            {
+                synthesizer.Dispose();
+                timer1.Dispose();
+                Application.Exit();
+            }
         }
     }
 }
